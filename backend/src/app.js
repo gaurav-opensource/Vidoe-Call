a7dc87bf4cdedd5bb2b0f8e592e2e42dd1909cb1
gaurav-opensource <157312982+gaurav-opensource@users.noreply.github.com>
2024-10-12 18:42:31 +0530

    Update app.js
    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,41 +1,3 @@
-// import express from "express";
-// import { createServer } from "node:http";
-
-// import { Server } from "socket.io";
-
-// import mongoose from "mongoose";
-// import { connectToSocket } from "./controllers/socketManager.js";
-
-// import cors from "cors";
-// import userRoutes from "./routes/users.routes.js";
-
-// const app = express();
-// const server = createServer(app);
-// const io = connectToSocket(server);
-
-
-// const { MONGO_URL, PORT } = process.env;
-// app.use(cors());
-// app.use(express.json({ limit: "40kb" }));
-// app.use(express.urlencoded({ limit: "40kb", extended: true }));
-
-// app.use("/api/v1/users", userRoutes);
-
-// // Connect to MongousersDB
-// mongoose
-//   .connect(MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB is connected successfully"))
-//   .catch((err) => console.error(err));
-
-// // // Middleware
-// // app.use(cors({
-// //   origin: ["http://localhost:3000"], // Adjust the origin to your frontend URL
-// //   methods: ["GET", "POST", "PUT", "DELETE"],
-// //   credentials: true,
-// // }));
 
 import express from "express";
 import mongoose from "mongoose";
@@ -58,7 +20,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async ()=>{
   app.set("mongo_user")
-  const connectionDb = await mongoose.connect("mongodb+srv://gauravwithhost1:Gauravyadav95@zoom.cdyoi.mongodb.net/?retryWrites=true&w=majority&appName=zoom")
+  const connectionDb = await mongoose.connect("mongodb+srv://Netfilix:GauravYadav95@netfilix.wivg6.mongodb.net/?retryWrites=true&w=majority&appName=Netfilix")
 
   console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
   server.listen(app.get("port"),
