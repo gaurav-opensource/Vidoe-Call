774fe5beca74a9d6c66cd1c5acf2a6aac1e78e3f
Gaurav Yadav <157312982+gaurav-opensource@users.noreply.github.com>
2025-06-13 10:48:40 +0530

    Update App.js
    update app.js // frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LandingPage from './pages/landing';
-import Authentication from './pages/authentication';
-import { AuthProvider } from './contexts/AuthContext';
+// import LandingPage from './pages/landing';
+// import Authentication from './pages/authentication';
+// import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
-import HomeComponent from './pages/home';
-import History from './pages/history';
+// import HomeComponent from './pages/home';
+// import History from './pages/history';
 
 function App() {
   return (
@@ -13,20 +13,20 @@ function App() {
 
       <Router>
 
-        <AuthProvider>
+      
 
 
           <Routes>
 
-            <Route path='/' element={<LandingPage />} />
+            // <Route path='/' element={<LandingPage />} />
 
-            <Route path='/auth' element={<Authentication />} />
+            // <Route path='/auth' element={<Authentication />} />
 
-            <Route path='/home's element={<HomeComponent />} />
-            <Route path='/history' element={<History />} />
+            // <Route path='/home's element={<HomeComponent />} />
+            // <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
-        </AuthProvider>
+     
 
       </Router>
     </div>
