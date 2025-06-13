5dfd3a34712f5d067fa8a1e759e80013010e952f
Gaurav Yadav <157312982+gaurav-opensource@users.noreply.github.com>
2025-06-13 10:49:34 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { createServer } from "node:http"; //connect both express and soket io
 import { Server } from "socket.io";
-import userRoutes from "./routes/users.routes.js";
+// import userRoutes from "./routes/users.routes.js";
 import { connectToSocket } from "./controllers/socketManager.js";
 const app = express();
 const server = createServer(app);
@@ -16,7 +16,7 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
-app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/users", userRoutes);
 
 const start = async ()=>{
   app.set("mongo_user")
